345eed946c0284e6b40df91f54698019bb271217
gaeguul <jeeham0226@gmail.com>
2024-08-08 17:55:11 +0900

    feat: 경의중앙선 역들 순서에 맞게 정렬하기 위해 상수데이터 LINE_GYEONGUI_DATA 추가

--- src/apis/services/getLineList.js
+++ src/apis/services/getLineList.js
@@ -1,3 +1,5 @@
+import LINE_GYEONGUI_DATA from "../../data/lineGyeonguiData";
+
 /**
  * 주어진 raw 데이터를 가공하여 노선에 따른 역 정보를 반환한다.
  * @param {Array} rawData 원시 데이터 배열
@@ -5,7 +7,8 @@
  */
 const getLineList = (rawData) => {
   const lines = {};
-  let uniqueLineId = 1; // rawData에서 받은 값에서 받은 lineId가 중복되는 경우가 발생해서 자체적으로 unique값을 생성했다.
+  let uniqueLineId = 1; // rawData에서 lineId가 중복되는 경우가 발생해서 자체적으로 unique값을 생성했다.
+
   rawData.forEach((station) => {
     const {
       stationId,
@@ -31,6 +34,9 @@ const getLineList = (rawData) => {
       stationName,
     });
   });
+
+  /** 경의중앙선의 경우, stations 값에 상수데이터를 추가한다 */
+  lines["경의중앙선"].stations = LINE_GYEONGUI_DATA;
 
   // 가공된 노선 정보를 배열로 반환
   return Object.values(lines);
