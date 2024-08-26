d5fa80dd8c57de7620f84a7213cd79cadf01a66a
gaeguul <jeeham0226@gmail.com>
2024-08-26 15:24:30 +0900

    refactor: React 프로젝트를 Typescript으로 마이그레이션

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./assets/styles/scss/style.scss";
 
 const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <QueryClientProvider client={queryClient}>
     <App />
