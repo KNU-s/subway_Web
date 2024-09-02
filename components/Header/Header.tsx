d17fb654a030bb0630befe810b3c62001ef6e1eb
gaeguul <jeeham0226@gmail.com>
2024-09-02 22:05:58 +0900

    feat: Header 컴포넌트에 뒤로가기 버튼 추가 및 제목 속성 추가

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,28 @@
-const Header = () => {
+import { useRouter } from 'next/router';
+import { GoChevronLeft } from 'react-icons/go';
+
+type HeaderProps = {
+  title: string;
+  showBackButton?: boolean; // 뒤로가기 버튼 표시할지 여부
+};
+
+const BackButton = () => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.back();
+  };
   return (
-    <div className="home__header">
-      <h1 className="home__title">노선</h1>
+    <div className="header__back-button" onClick={handleClick}>
+      <GoChevronLeft />
+    </div>
+  );
+};
+
+const Header = ({ title, showBackButton = false }: HeaderProps) => {
+  return (
+    <div className="header">
+      {showBackButton && <BackButton />}
+      <h1 className="header__title">{title}</h1>
     </div>
   );
 };
