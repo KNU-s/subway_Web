e2cfe091dc5375ce581d53a2bb6b49331a42a874
gaeguul <jeeham0226@gmail.com>
2024-09-02 22:06:44 +0900

    feat: LineItem 컴포넌트에서 노선 이름을 URL로 인코딩하여 링크 생성

--- components/LineItem/LineItem.tsx
+++ components/LineItem/LineItem.tsx
@@ -6,9 +6,10 @@ type LineItemProps = {
 };
 
 const LineItem = ({ line }: LineItemProps) => {
+  const encodedLineFullName = encodeURIComponent(line.lineFullName);
   const lineItemClassName = `line-item line-item__${line.lineId}`;
   return (
-    <Link href={`/line/${line.lineUrlId}`}>
+    <Link href={`/line/${encodedLineFullName}`}>
       <div className={lineItemClassName}>
         {line.lineFullName}
         <div className={`line-item__thumb`} />
