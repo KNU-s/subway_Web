e01afeb1df7e9138423e50cdc6f93883313b85d0
gaeguul <jeeham0226@gmail.com>
2024-09-02 13:23:31 +0900

    Header와 LineList 컴포넌트 추가

---
+++ components/Header/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="home__header">
+      <h1 className="home__title">노선</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/LineList/LineList.tsx
@@ -0,0 +1,12 @@
+const LineList = () => {
+  // 모든 노선 가져오기
+  return (
+    <div className="line-list">
+      {/* {lineList.map((line) => (
+          <LineItem key={line.uniqueLineId} line={line} />
+        ))} */}
+    </div>
+  );
+};
+
+export default LineList;
