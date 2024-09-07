6a2835884c92e3c2e4f6ae473adaed2d6adccee8
gaeguul <jeeham0226@gmail.com>
2024-09-07 15:53:08 +0900

    feat: SVG 컴포넌트 사용하기 위해 svgr/webpack 패키지 설치 및 webpack.config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,22 @@ const nextConfig = {
   sassOptions: {
     includePaths: ['styles'],
   },
+
+  webpack(config, options) {
+    // SVG 파일을 React 컴포넌트로 변환하는 설정 추가
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            icon: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
