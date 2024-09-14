1a8329bb94f5d3c2bcbd1bc227e05b8ae660ed82
gaeguul <jeeham0226@gmail.com>
2024-09-14 15:16:47 +0900

    feat: 스크롤 시 Header 숨김 기능 구현

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { GoChevronLeft } from 'react-icons/go';
 
 const BackButton = () => {
@@ -14,10 +15,31 @@ const BackButton = () => {
 };
 
 const Header = () => {
+  const [isHidden, setIsHidden] = useState(false);
+  let lastScrollY = 0;
+  const headerClassName = isHidden ? `header header--hidden` : `header`;
+
   const router = useRouter();
   const title = typeof router.query.lineName === 'string' ? router.query.lineName : '노선';
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY) {
+        setIsHidden(true);
+      } else {
+        setIsHidden(false);
+      }
+      lastScrollY = currentScrollY;
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='header'>
+    <div className={headerClassName}>
       <BackButton />
       <h1 className='header__title'>{title}</h1>
     </div>
