0b01dcc1b2f16dbc679c22a0a513927d7a829b95
gaeguul <jeeham0226@gmail.com>
2024-09-21 19:30:19 +0900

    feat: 메타 데이터 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,28 @@
-import { Layout } from '@/components/Layout';
+import Layout from '@/components/Layout';
 import { LineProvider } from '@/context/LineContext';
 import '@/styles/index.scss';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <LineProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </LineProvider>
+    <>
+      <Head>
+        <title>열차영차</title>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='apple-touch-icon' href='/logo192.png' />
+        <meta charSet='UTF-8' />
+        <meta name='description' content='실시간 지하철 위치 확인 웹 서비스' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#f5f5f5' />
+      </Head>
+      <LineProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </LineProvider>
+    </>
   );
 };
 
